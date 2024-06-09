42ff3f3591d947a0aa0de953f03f7c575cdee145
Dimas Izzulhaq Zain <124556325+Diz09@users.noreply.github.com>
2024-06-09 11:17:02 +0700

    fix forgot password

--- lib/pages/forgot_password.dart
+++ lib/pages/forgot_password.dart
@@ -1,8 +1,13 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import '../../core/app_export.dart';
-import '../../theme/custom_button_style.dart';
-import '../../widgets/custom_elevated_button.dart';
-import '../../widgets/custom_pin_code_text_field.dart';
+import 'package:vioscake_admin/core/app_export.dart';
+import 'package:vioscake_admin/pages/login_page.dart';
+import 'package:vioscake_admin/theme/custom_button_style.dart';
+import 'package:vioscake_admin/widgets/custom_elevated_button.dart';
+import 'package:vioscake_admin/widgets/custom_pin_code_text_field.dart';
+
+import 'package:http/http.dart' as http;
 
 class ForgotPassword extends StatelessWidget {
   const ForgotPassword({Key? key})
@@ -12,6 +17,49 @@ class ForgotPassword extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    TextEditingController txtEmail = TextEditingController();
+
+    Future<void> changePassword(BuildContext context, String email) async {
+      final url = Uri.parse('https://vioscake.my.id/api/forgot-password');
+      final headers = {
+        'Content-Type' : 'application/json',
+        // 'User' : 'Mobile'
+      };
+      final body = jsonEncode({
+        'email' : email,
+      });
+      final response = await http.post(
+        url,
+        headers: headers,
+        body: body,
+      );
+      if (response.statusCode == 200) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => LoginPage()),
+        );
+      } else {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text('Error'),
+              content: Text('Email not found'),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text('OK'),
+                ),
+              ],
+            );
+          },
+        );
+      }
+    }
+    
     return SafeArea(
       child: Scaffold(
         // backgroundColor: theme.colorScheme.onPrimary,
@@ -36,44 +84,61 @@ class ForgotPassword extends StatelessWidget {
                 style: CustomTextStyles.displaySmallOnPrimaryContainer,
               ),
               SizedBox(height: 15.v),
-              Text(
-                "Please enter the code we just send to email",
-                style: CustomTextStyles.bodySmallRegular,
-              ),
-              SizedBox(height: 13.v),
-              Text(
-                "xxxxx@gmail.com",
-                style: CustomTextStyles.bodySmallOnPrimaryContainerRegular,
-              ),
-              SizedBox(height: 38.v),
-              Padding(
-                padding: EdgeInsets.only(
-                  left: 49.h,
-                  right: 51.h,
+              Container(
+                margin: EdgeInsets.only(top: 15),
+                padding: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 10.0,
                 ),
-                child: CustomPinCodeTextField(
-                  context: context,
-                  onChanged: (value) {},
-                ),
-              ),
-              SizedBox(height: 40.v),
-              RichText(
-                text: TextSpan(
-                  children: [
-                    TextSpan(
-                      text: "If you didn’t receive the code? ",
-                      style: CustomTextStyles.bodySmallOnPrimaryContainerRegular_1,
+                child: TextField(
+                  controller: txtEmail,
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20.0),
                     ),
-                    TextSpan(
-                      text: "Resend",
-                      style: CustomTextStyles.labelLargeGray7000112.copyWith(
-                        decoration: TextDecoration.underline,
-                      ),
-                    )
-                  ],
+                  ),
                 ),
-                textAlign: TextAlign.left,
               ),
+              // SizedBox(height: 10.v),
+              // Text(
+              //   "Please enter the code we just send to email",
+              //   style: CustomTextStyles.bodySmallRegular,
+              // ),
+              // SizedBox(height: 13.v),
+              // Text(
+              //   "xxxxx@gmail.com",
+              //   style: CustomTextStyles.bodySmallOnPrimaryContainerRegular,
+              // ),
+              // SizedBox(height: 38.v),
+              // Padding(
+              //   padding: EdgeInsets.only(
+              //     left: 49.h,
+              //     right: 51.h,
+              //   ),
+              //   child: CustomPinCodeTextField(
+              //     context: context,
+              //     onChanged: (value) {},
+              //   ),
+              // ),
+              // SizedBox(height: 40.v),
+              // RichText(
+              //   text: TextSpan(
+              //     children: [
+              //       TextSpan(
+              //         text: "If you didn’t receive the code? ",
+              //         style: CustomTextStyles.bodySmallOnPrimaryContainerRegular_1,
+              //       ),
+              //       TextSpan(
+              //         text: "Resend",
+              //         style: CustomTextStyles.labelLargeGray7000112.copyWith(
+              //           decoration: TextDecoration.underline,
+              //         ),
+              //       )
+              //     ],
+              //   ),
+              //   textAlign: TextAlign.left,
+              // ),
               SizedBox(height: 48.v),
               CustomElevatedButton(
                 text: "Continue",
@@ -84,7 +149,8 @@ class ForgotPassword extends StatelessWidget {
                 buttonStyle: CustomButtonStyles.fillGray,
                 buttonTextStyle: theme.textTheme.titleLarge!,
                 onPressed: () {
-                  onTapContinue(context);
+                  // onTapContinue(context);
+                  changePassword(context, txtEmail.text,);
                 },
               ),
               SizedBox(height: 5.v)
